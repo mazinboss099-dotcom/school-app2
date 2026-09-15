c4acc0a4f8b463b8c7ce52cc0d34cec04a035160
mazin0120x-gif <_>
2026-09-15 21:06:32 +0300

    debug: log dirname

--- server.js
+++ server.js
@@ -26,10 +26,13 @@ const io = new Server(server, { cors: { origin: '*' } });
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, '../frontend')));
+console.log('DIRNAME IS:', __dirname);
+console.log('FRONTEND PATH WOULD BE:', path.join(__dirname, '..', 'frontend'));
+
+app.use(express.static(path.join(__dirname, '..', 'frontend')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/admin/index.html'));
+  res.sendFile(path.join(__dirname, '..', 'frontend', 'admin', 'index.html'));
 });
 
 function requireAdmin(req, res, next) {
